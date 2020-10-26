68a91d7d8c889238da2e7c3c372d4a373017956e
Aquat1c <49980958+Aquat1c@users.noreply.github.com>
2020-10-26 19:30:59 +0200

    Add files via upload

---
+++ 3.4/3.4.cpp
@@ -0,0 +1,29 @@
+﻿.Текст програми
+//  Lab_03_4.cpp
+//  Гудима Михайло
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 5
+#include  <iostream> 
+#include <math.h>
+#define M_PI
+using  namespace  std;
+int main()
+{
+	double  x;	//  вхідний  аргумент 
+	double  y;	//  вхідний  параметр
+	double R;
+
+
+	cout << "x  =  ";  cin >> x; cout << "y  =  ";  cin >> y; cout << "R  =  ";  cin >> R;
+
+	//  розгалуження  в  повній  формі
+	if ((y <= sqrt(2 * R * R) && y >= -R) ||
+		(y <= M_PI * R / 2. && y >= R) ||
+		(y <= -R && y >= M_PI * R / 2.))
+		cout << "yes" << endl;
+	else
+
+		cout << "no" << endl;
+
+	cin.get(); return  0;
